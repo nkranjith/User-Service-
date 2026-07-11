b1d63b427e047e875f1d90913dfece11051f7a94
Ranjith NK <ranjithnkranju@gmail.com>
2026-07-11 09:43:50 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    _tablename_ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
